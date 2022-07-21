c27d29d4ce4848cb5e055a463a351d374288a19b
Nicola <ziapo98@gmail.com>
2022-07-21 12:03:07 +0200

    Estimated whole model W from refPos and outputPos, tried to extract C from it using algebraic operations, compare the extracted C with the estimated C from posError and torque, further work to be done

--- concat_estimation.m
+++ concat_estimation.m
@@ -100,7 +100,8 @@ torqueEpsilon=0.2;
 
 maxSignalLength = 140;
 
-plot_signals=true;
+C_plot_signals=true;
+W_plot_signals=true;
 
 % variable which if set to true enables method 3
 flip_1_every_2=false;
@@ -110,9 +111,13 @@ flip_1_every_2=false;
 allTrimmedTorque = {};
 allTrimmedPosError = {};
 
+
+allTrimmedPos = {};
+allTrimmedRefPos={};
+
 i = 1;
 
-if plot_signals
+if C_plot_signals
     figure; 
     hAx1 = subplot(2,1,1);
     title('Position error of the experiments') 
@@ -130,6 +135,27 @@ if plot_signals
     axes(hAx3)
     hold on
     axes(hAx4)
+    hold on
+end
+
+if W_plot_signals
+    figure; 
+    hAx5 = subplot(2,1,1);
+    title('Ref position of the experiments') 
+    hAx6 = subplot(2,1,2);
+    title('Position output from experiments')
+    figure; 
+    hAx7 = subplot(2,1,1);
+    title('FILT: Ref position error of the experiments') 
+    hAx8 = subplot(2,1,2);
+    title('FILT: Position output from experiments')
+    axes(hAx5)
+    hold on
+    axes(hAx6)
+    hold on
+    axes(hAx7)
+    hold on
+    axes(hAx8)
     hold on
 end
 
@@ -167,15 +193,26 @@ for expId = selected_indeces  %for each experiment
     if abs(trimmedTorque(end))>torqueEpsilon
         continue;
     end
-    if plot_signals
+    
+    if C_plot_signals
         plot(hAx1,ones(length(position),1)*referencePosVal-position);
         plot(hAx2,torque);
         plot(hAx3,trimmedPosErr);
         plot(hAx4,trimmedTorque);
     end
     
+    if W_plot_signals
+        plot(hAx5,ones(length(position),1)*referencePosVal);
+        plot(hAx6,position);
+        plot(hAx7,trimmedRefPos);
+        plot(hAx8,trimmedPos);
+    end
+    
     allTrimmedTorque{i} = trimmedTorque;
     allTrimmedPosError{i} = trimmedPosErr;
+    allTrimmedPos{i} = trimmedPos;
+    allTrimmedRefPos{i} = trimmedRefPos;
+    
     i = i + 1;
 end
 
@@ -183,12 +220,21 @@ clear expId i;
 
 %% Construct estimation and testing set
 
+%control estimation
 estimationPosError={};
 estimationTorque={};
 
 testingPosError={};
 testingTorque={};
 
+%whole model estimation 
+estimationRefPos={};
+estimationPos={};
+
+testingRefPos={};
+testingPos={};
+
+% Choosing randomly the testing experiments
 numOfTest=ceil(length(allTrimmedTorque)*0.2);
 
 % vector containig the testing indeces, randperm guarantees distinct
@@ -199,15 +245,28 @@ testing_indeces=randperm(length(allTrimmedTorque),numOfTest);
 deprecatedTrimmedPosError=allTrimmedPosError;
 deprecatedTrimmedTorque=allTrimmedTorque;
 
+deprecatedTrimmedRefPos=allTrimmedRefPos;
+deprecatedTrimmedPos=allTrimmedPos;
+
 i=1;
 for idx = testing_indeces  %for each experiment    
+    
+    % controller
     testingPosError{i} = allTrimmedPosError{idx};
     testingTorque{i} = allTrimmedTorque{idx};
     
-    i=i+1;
-    
     deprecatedTrimmedPosError{idx}=[];
     deprecatedTrimmedTorque{idx}=[];
+    
+    % whole model
+    testingRefPos{i} = allTrimmedRefPos{idx};
+    testingPos{i} = allTrimmedPos{idx};
+    
+    deprecatedTrimmedRefPos{idx}=[];
+    deprecatedTrimmedPos{idx}=[];
+    
+    
+    i=i+1;
 end
 
 clear i idx;
@@ -220,58 +279,90 @@ for idx = 1:1:length(deprecatedTrimmedPosError)
        continue;
    end
    if flip_1_every_2
+        % method 3
        if mod(i,2)==0
            estimationPosError{i}=flip(deprecatedTrimmedPosError{idx});
            estimationTorque{i}=flip(deprecatedTrimmedTorque{idx});
+           
+           estimationRefPos{i} = flip(deprecatedTrimmedRefPos{idx});
+           estimationPos{i} = flip(deprecatedTrimmedPos{idx}); 
        else 
            estimationPosError{i}=deprecatedTrimmedPosError{idx};
            estimationTorque{i}=deprecatedTrimmedTorque{idx};   
+           
+           estimationRefPos{i} = deprecatedTrimmedRefPos{idx};
+           estimationPos{i} = deprecatedTrimmedPos{idx}; 
        end
    else
-       estimationPosError{i}=deprecatedTrimmedPosError{idx};
-       estimationTorque{i}=deprecatedTrimmedTorque{idx};    
+       % controller
+       estimationPosError{i} = deprecatedTrimmedPosError{idx};
+       estimationTorque{i} = deprecatedTrimmedTorque{idx}; 
+       
+       % whole model
+       estimationRefPos{i} = deprecatedTrimmedRefPos{idx};
+       estimationPos{i} = deprecatedTrimmedPos{idx}; 
+
    end
    i=i+1;
 end
 
 clear i idx;
 clear deprecatedTrimmedPosError deprecatedTrimmedTorque;
+clear deprecatedTrimmedRefPos deprecatedTrimmedPos; 
 
 % Number of poles and zeros for the controller
-num_zeros = 2;
-num_poles = 2;
+C_num_zeros = 2;
+C_num_poles = 2;
+
+W_num_zeros = C_num_zeros;
+W_num_poles = C_num_poles + 2;
 
 % construct iddata structure for testing
-testing_iddata={};
-allTestingTorque=[];
+C_testing_iddata={};
+W_testing_iddata={};
+
 for testIdx=1:1:length(testingTorque)
-    testing_iddata{testIdx} = iddata(testingTorque{testIdx}, testingPosError{testIdx}, Ts); 
-    allTestingTorque=[allTestingTorque;testingTorque{testIdx}]; 
+    C_testing_iddata{testIdx} = iddata(testingTorque{testIdx}, testingPosError{testIdx}, Ts); 
+    W_testing_iddata{testIdx} = iddata(testingPos{testIdx}, testingRefPos{testIdx}, Ts); 
 end
 
 clear testIdx;
 
-% Method 1) Controller estimation using the single experiments
+% Method 1) Model estimation using the single experiments
 C_sys_est={};
+W_sys_est={};
 for expIdx=1:1:length(estimationPosError)
-    est_iddata = iddata(estimationTorque{expIdx}, estimationPosError{expIdx}, Ts); 
-    C_sys_est{expIdx} = tfest(est_iddata, num_poles, num_zeros); 
+    C_est_iddata = iddata(estimationTorque{expIdx}, estimationPosError{expIdx}, Ts); 
+    C_sys_est{expIdx} = tfest(C_est_iddata, C_num_poles, C_num_zeros); 
+    
+    W_est_iddata = iddata(estimationRefPos{expIdx}, estimationPos{expIdx}, Ts); 
+    W_sys_est{expIdx} = tfest(W_est_iddata, W_num_poles, W_num_zeros); 
 end
 
 clear expIdx;
 
-[bestModelSingle, bestModelFitSingle, bestModelOutputSingle] = bestModelFinder(C_sys_est, testing_iddata);
+[C_bestModelSingle, C_bestModelFitSingle, C_bestModelOutputSingle] = bestModelFinder(C_sys_est, C_testing_iddata);
+
+[W_bestModelSingle, W_bestModelFitSingle, W_bestModelOutputSingle] = bestModelFinder(W_sys_est, W_testing_iddata);
 
 % Method 2) Controller estimation using the 80% of experiments
-estPosError80=[];
-estTorque80=[];
-C_sys_est_80={};
+estPosError80 = [];
+estTorque80 = [];
+C_sys_est_80 = {};
+
+estPos80 = [];
+estRefPos80 = [];
+W_sys_est_80 = {};
+
 for expIdx=1:1:length(estimationPosError)
     estPosError80 = [estPosError80; estimationPosError{expIdx}];
     estTorque80 = [estTorque80; estimationTorque{expIdx}];
+    
+    estPos80 = [estPos80; estimationPos{expIdx}];
+    estRefPos80 = [estRefPos80; estimationRefPos{expIdx}];
 end
 
-if plot_signals
+if C_plot_signals
     figure; 
     hAx1 = subplot(2,1,1);
     title('CONCATENATED Estimation Position error') 
@@ -285,50 +376,123 @@ if plot_signals
     plot(hAx2,estTorque80);
 end
 
+if W_plot_signals
+    figure; 
+    hAx1 = subplot(2,1,1);
+    title('CONCATENATED Estimation Ref Position') 
+    hAx2 = subplot(2,1,2);
+    title('CONCATENATED Estimation Position')
+    axes(hAx1)
+    hold on
+    axes(hAx2)
+    hold on
+    plot(hAx1,estRefPos80);
+    plot(hAx2,estPos80);
+end
+
 clear expIdx;
 
 
-C_sys_est_80{1} = tfest(iddata(estTorque80, estPosError80, Ts), num_poles, num_zeros); 
+C_sys_est_80{1} = tfest(iddata(estTorque80, estPosError80, Ts), C_num_poles, C_num_zeros); 
+W_sys_est_80{1} = tfest(iddata(estPos80, estRefPos80, Ts), W_num_poles, W_num_zeros); 
 
-[bestModel80, bestModelFit80, bestModelOutput80] = bestModelFinder(C_sys_est_80, testing_iddata);
+[C_bestModel80, C_bestModelFit80, C_bestModelOutput80] = bestModelFinder(C_sys_est_80, C_testing_iddata);
+[W_bestModel80, W_bestModelFit80, W_bestModelOutput80] = bestModelFinder(W_sys_est_80, W_testing_iddata);
 
 % RESULTS
 clc
-fprintf('\nBest model fit from single experiment estimation: %.3f\n', bestModelFitSingle);
-fprintf('Model fit from using 80 experiments for estimation: %.3f\n\n', bestModelFit80);
+fprintf('\nC: Best model fit from single experiment estimation: %.3f\n', C_bestModelFitSingle);
+fprintf('C: Model fit from using 80 experiments for estimation: %.3f\n\n', C_bestModelFit80);
 
-% if(bestModelFitSingle>bestModelFit80)
-%     bestModelSingle
+fprintf('\nW: Best model fit from single experiment estimation: %.3f\n', W_bestModelFitSingle);
+fprintf('W: Model fit from using 80 experiments for estimation: %.3f\n\n', W_bestModelFit80);
+
+% if(C_bestModelFitSingle>C_bestModelFit80)
+%     C_bestModelSingle
 % else
-%     bestModel80
+%     C_bestModel80
 % end
 
 close all
 
-for i=1:1:length(testing_iddata)
-    figure
-    titleStr=['Test ',num2str(i)]; 
-    if(bestModelFitSingle>bestModelFit80)
-        titleStr=['Single is better - ', titleStr];
-    else
-        titleStr=['80% is better - ', titleStr];
+%controller
+if C_plot_signals
+    for i=1:1:length(C_testing_iddata)
+        figure
+        titleStr=['Test ',num2str(i)]; 
+        if(C_bestModelFitSingle>C_bestModelFit80)
+            titleStr=['Single is better - ', titleStr];
+        else
+            titleStr=['80% is better - ', titleStr];
+        end
+        if flip_1_every_2
+            titleStr=['flip1every2 - ', titleStr]; 
+        end
+        titleStr=['C - ', titleStr]; 
+
+        sgtitle(titleStr); 
+        subplot(2,1,1)
+        hold on;
+        title('Position of best W estimated with a single experiment') 
+        plot(0:length(testingTorque{i})-1,testingTorque{i});
+        plot(0:length(C_bestModelOutputSingle{i})-1,C_bestModelOutputSingle{i});
+        legend('testing', 'estimated model output')
+        subplot(2,1,2)
+        hold on;
+        title('Position of W estimated with 80% of experiments') 
+        plot(0:length(testingTorque{i})-1,testingTorque{i});
+        plot(0:length(C_bestModelOutput80{i})-1,C_bestModelOutput80{i});
+        legend('testing', 'estimated model output')
     end
-    if flip_1_every_2
-        titleStr=['flip1every2 - ', titleStr]; 
+end
+
+clear i;
+
+%whole model
+if W_plot_signals
+    for i=1:1:length(W_testing_iddata)
+        figure
+        titleStr=['Test ',num2str(i)]; 
+        if(W_bestModelFitSingle>W_bestModelFit80)
+            titleStr=['Single is better - ', titleStr];
+        else
+            titleStr=['80% is better - ', titleStr];
+        end
+        if flip_1_every_2
+            titleStr=['flip1every2 - ', titleStr]; 
+        end
+        titleStr=['W - ', titleStr]; 
+
+        sgtitle(titleStr); 
+        subplot(2,1,1)
+        hold on;
+        title('Position best W estimated with a single experiment') 
+        plot(0:length(testingPos{i})-1,testingPos{i});
+        plot(0:length(W_bestModelOutputSingle{i})-1,W_bestModelOutputSingle{i});
+        legend('testing', 'estimated model output')
+        subplot(2,1,2)
+        hold on;
+        title('Position of W estimated with 80% of experiments') 
+        plot(0:length(testingPos{i})-1,testingPos{i});
+        plot(0:length(W_bestModelOutput80{i})-1,W_bestModelOutput80{i});
+        legend('testing', 'estimated model output')
     end
-   
-    sgtitle(titleStr); 
-    subplot(2,1,1)
-    hold on;
-    title('Torque of best C estimated with a single experiment') 
-    plot(0:length(testingTorque{i})-1,testingTorque{i});
-    plot(0:length(bestModelOutputSingle{i})-1,bestModelOutputSingle{i});
-    legend('testing', 'estimated model output')
-    subplot(2,1,2)
-    hold on;
-    title('Torque of C estimated with 80% of experiments') 
-    plot(0:length(testingTorque{i})-1,testingTorque{i});
-    plot(0:length(bestModelOutput80{i})-1,bestModelOutput80{i});
-    legend('testing', 'estimated model output')
 end
 clear i;
+
+%% Extracting controllers from estimated W and compare them with the estimated C
+clc;
+s = tf('s');
+j = 0.068;
+d = 0.01;
+G = 1/(j*s^2 + d*s);
+
+C_single = invC(G, W_bestModelSingle);
+newC_Single = zpk(minreal(C_single,0.5))
+C_BM_Single = zpk(C_bestModelSingle)
+
+C_80 = invC(G, W_bestModel80);
+newC_80 = zpk(minreal(C_80,0.5))
+C_BM_80 = zpk(C_bestModel80)
+
+%%
